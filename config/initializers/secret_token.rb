1a165cd4bd24870293fa7feb7db2e92fc35df227
Gregory Eremin <magnolia_fan@me.com>
2013-03-18 00:04:35 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClearanceBugDemo::Application.config.secret_key_base = '779a1dca5f582246e49a0b11adac9c298bac73ee2befb4476d18ff61068cefcbcf289738bc4a7beed4c33b28cd89e90126b31f176a64aca0f3c202977d0bd0fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ClearanceBugDemo::Application.config.session_store :encrypted_cookie_store, key: '_clearance_bug_demo_session'
